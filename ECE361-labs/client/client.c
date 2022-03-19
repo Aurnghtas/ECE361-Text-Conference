772f41ea2668095dc41ba958b8fade3b084c4ee5
Jiayi Zhou <jysteven.zhou@mail.utoronto.ca>
2022-03-19 04:44:30 -0400

    modified client.c and message.h by Jiayi Zhou

--- ECE361-labs/client/client.c
+++ ECE361-labs/client/client.c
@@ -3,8 +3,174 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <../message.h>
 
-int main(){
-    printf("Hello world.");
+#define MAX_INPUT_LEN 1024
+
+/*******************
+ * Client Commands *
+ *******************/
+const char* login = "/login";
+const char* logout = "/logout";
+const char* joinsession = "/joinsession";
+const char* leavesession = "/leavesession";
+const char* createsession = "/createsession";
+const char* list = "/list";
+const char* quit = "/quit";
+
+/*************************************************************************** 
+ * some parts of the code are cited from https://beej.us/guide/bgnet/html/ *
+ ***************************************************************************/
+int main(int argc, char* argv[]){
+    if(argc != 1){
+        printf("Usage: client\n");
+        exit(1);
+    }
+
+    char user_input[MAX_INPUT_LEN];
+    char* command;
+    int socketfd = -2;
+
+    while(true){
+        Message msg;
+        char msg_buffer[MAX_MSG_TO_STRING];
+        char recv_buffer[MAX_MSG_TO_STRING];
+        memset(user_input, '\0', sizeof(char)*MAX_INPUT_LEN);
+        fgets(user_input, MAX_INPUT_LEN, stdin);
+        command = strtok(user_input, " ");
+
+        /* login command */
+        if(strcmp(command, login)==0) {
+            if(socketfd!=-2) {
+                printf("An client ID is already in use, please quit the previous client ID first\n");
+                continue;
+            }
+
+            char* client_ID, pwd, server_IP, server_port;
+
+            /* invalid usage of login */
+            if((client_ID = strtok(NULL, " "))==NULL){
+                printf("Usage for login: /login <client ID> <password> <server-IP> <server-port>\n");
+                continue;
+            }
+            if((pwd = strtok(NULL, " "))==NULL){
+                printf("Usage for login: /login <client ID> <password> <server-IP> <server-port>\n");
+                continue;
+            }
+            if((server_IP = strtok(NULL, " "))==NULL){
+                printf("Usage for login: /login <client ID> <password> <server-IP> <server-port>\n");
+                continue;
+            }
+            if((server_port = strtok(NULL, " "))==NULL){
+                printf("Usage for login: /login <client ID> <password> <server-IP> <server-port>\n");
+                continue;
+            }
+
+            /*!!!!!!!!!!!!!!!! Only For Debug Purpose !!!!!!!!!!!!!!!!*/
+            printf("inside login command, we have %s %s %s %s\n", client_ID, pwd, server_IP, server_port);
+
+            /* establish the connection */
+            int rv;
+            struct addrinfo hints, *res;
+            memset(&hints, 0, sizeof hints);    // using memset to initialize struct
+            hints.ai_family = AF_INET;  // IPv4 internet protocols
+            hints.ai_socktype = SOCK_STREAM;     // socket is TCP
+            hints.ai_flags = AI_PASSIVE;    // fill in my IP for me
+
+            /* get IP address */
+            if((rv = getaddrinfo(server_IP, server_port, &hints, &res)) != 0) {
+                fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+                exit(1);
+            }
+
+            /* get the socketfd */
+            if((socketfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) == -1) {
+                printf("Failed to create a socket in login\n");
+                exit(1);
+            }
+
+            /* connect to the server */
+            if(connect(socketfd, res->ai_addr, res->ai_addrlen)==-1) {
+                printf("Failed to connect to the server\n");
+                exit(1);
+            }
+            
+            /* send Message to the server */
+            msg.type = LOGIN;
+            strcpy(msg.source, client_ID);
+            strcpy(msg.data, pwd);
+            msg.size = strlen(msg.data);
+
+            messageToStrings(msg, msg_buffer);
+            if(send(socketfd, msg_buffer, strlen(msg_buffer), 0)==-1){
+                printf("Error in sending the Message to the server\n");
+                continue;
+            }
+
+            /* receive ack from the server */
+            int num_bytes_recv = recv(socketfd, recv_buffer, strlen(recv_buffer), 0);
+            if(num_bytes_recv==-1) {
+                printf("Error in receiving the Message from the server\n");
+            }
+
+            recv_buffer[num_bytes_recv] = '\0'; // add the string terminator to the buffer
+
+            Message recv_msg = stringsToMessage(recv_buffer);
+            
+            if(recv_msg.type==LO_ACK) {
+                printf("Successfully log in with %s\n", client_ID);
+            } else if(recv_msg.type=LO_NAK) {
+                printf("Failed to log in with %s, reason: %s\n", client_ID, recv_msg.data);
+                socketfd=-2;
+            }
+
+        } 
+
+        /* logout command */
+        else if(strcmp(command, logout)==0) {
+
+        } 
+
+        /* joinsession command */
+        else if(strcmp(command, joinsession)==0) {
+
+        } 
+
+        /* leavesession command */
+        else if(strcmp(command, leavesession)==0) {
+            
+        } 
+
+        /* createsession command */
+        else if(strcmp(command, createsession)==0) {
+            
+        } 
+
+        /* list command */
+        else if(strcmp(command, list)==0) {
+            
+        } 
+
+        /* quit command */
+        else if(strcmp(command, quit)==0) {
+            
+        } 
+
+        /* text - not a command */
+        else {
+
+        }
+
+    }
+
+    // /*!!!!!!!!!!!!!!!! Only For Debug Purpose !!!!!!!!!!!!!!!!*/
+    // printf("\n");
+
 }
--- ECE361-labs/message.h
+++ ECE361-labs/message.h
@@ -4,8 +4,9 @@
 
 #define MAX_NAME 32
 #define MAX_DATA 1024
+#define MAX_MSG_TO_STRING 1064
 
-#define max_package_size 1000
+//#define max_package_size 1000
 
 //message types
 enum messageType{
@@ -25,103 +26,124 @@ enum messageType{
 };
 
 //the structure for messages in the conference lab
-typedef struct Message{
+typedef struct message{
     unsigned int type;  //refers to messageType
     unsigned int size;  //size of the data
     unsigned char source[MAX_NAME];  //ID of the client sending the message
     unsigned char data[MAX_DATA];
-}message;
+}Message;
 
-typedef struct packet{
-    unsigned int total_frag;
-    unsigned int frag_no;
-    unsigned int size;
-    char* filename;
-    char filedata[max_package_size];
-}Packet;
+// helper function converts Message to strings with size MAX_MSG_TO_STRING
+void messageToStrings(Message message, char* buffer) {
+    int num_char = sprintf(buffer, "%d:%d:%s:%s:", message.type, message.size, message.source, message.data);
+    if(num_char<0) {
+        printf("Error in messageToStrings\n");
+    }
+}
 
-//This is the helper function that parse the recieved message into packet,
-//it will create a new file if the message is the first one and close file after the last message
-int packet_from_message(char* message, int prev_index){
-    //first use strtok function to break up the message header
-    //referred to this tutorial https://www.cplusplus.com/reference/cstring/strtok/
-    FILE* FD = NULL;
-
+// helper function converts strings to Message
+Message stringsToMessage(char* buffer) {
+    Message message;
     const char breaker[2] = ":";
-    char* total_frag = strtok(message, breaker);
-    char* frag_no = strtok(NULL, breaker); //use null to continue scanning
-    char* size = strtok(NULL, breaker);
-    char* filename = strtok(NULL, breaker);
-
-    //now turn strings to int values
-    unsigned int Total_frag = atoi(total_frag);
-    unsigned int Frag_no = atoi(frag_no);
-    unsigned int dSize = atoi(size);
-
-    /*if(Frag_no != (prev_index) + 1){
-        return -1;
-    }*/
-
-    if(Frag_no == 1){
-        //this is the first message revieved, need to create a Packet struct and file stream
-        Packet recieved;
-        recieved.filename = filename;
-        recieved.frag_no = Frag_no;
-        recieved.size = dSize;
-        recieved.total_frag = Total_frag;
-
-        //create file stream, open a file
-        FD = fopen(filename, "wb"); //recieved file needs to have same name as sent one
-        if(FD == NULL){
-            printf("Cannot create file\n"); 
-            return -1; // -1 for error
-        }
-    }
-
-    //now, write data into the created file use fwrite to write to stream
-    int header_size = strlen(total_frag) + strlen(frag_no) + strlen(size) + strlen(filename);
-    //fwrite, notice need to skip the header
-    fwrite(message + header_size + sizeof(char) * 4, sizeof(char), dSize, FD);
-
-    //now check if the last message is done transmitting
-    if(Frag_no == Total_frag){
-        //fclose(fd);
-        return 1; // 1 for end of this file transmission
-    }
-
-    return 0; // 0 for success and continue this file transmission
+    message.type = atoi(strtok(buffer, breaker));
+    message.size = atoi(strtok(NULL, breaker)); //use null to continue scanning
+    strcpy(message.source, strtok(NULL, breaker));
+    strcpy(message.data, strtok(NULL, breaker));
+    return message;
 }
 
-void message_from_packet(Packet packet, char* message) {
-    int index = sprintf(message, "%d:%d:%d:%s:", packet.total_frag, 
-            packet.frag_no, packet.size, packet.filename);  // fill in everything except data region
-    for(int i=0; i<packet.size; i++) {
-        message[index+i] = packet.filedata[i];  // fill in the data region
-    }
-}
 
-void constructPacketsArray(Packet* array, int total_packets, char* data, char* fileName, int remaining_file) {
-    for(int index=0; index<total_packets; index++) {
-        array[index].total_frag = total_packets;
-        array[index].frag_no = index+1;
 
-        /* pay attention to the last packet's size */
-        if(index==total_packets-1 && remaining_file!=0) {
-            array[index].size = remaining_file;
-        } else {
-            array[index].size = max_package_size;
-        }
+// typedef struct packet{
+//     unsigned int total_frag;
+//     unsigned int frag_no;
+//     unsigned int size;
+//     char* filename;
+//     char filedata[max_package_size];
+// }Packet;
 
-        array[index].filename = fileName;
+// //This is the helper function that parse the recieved message into packet,
+// //it will create a new file if the message is the first one and close file after the last message
+// int packet_from_message(char* message, int prev_index){
+//     //first use strtok function to break up the message header
+//     //referred to this tutorial https://www.cplusplus.com/reference/cstring/strtok/
+//     FILE* FD = NULL;
 
-        if(index==total_packets-1 && remaining_file!=0) {
-            for(int i=0; i<remaining_file; i++) {
-                array[index].filedata[i] = data[i+index*max_package_size];
-            }
-        } else {
-            for(int i=0; i<max_package_size; i++) {
-                array[index].filedata[i] = data[i+index*max_package_size];
-            }
-        }
-    }
-}
+//     const char breaker[2] = ":";
+//     char* total_frag = strtok(message, breaker);
+//     char* frag_no = strtok(NULL, breaker); //use null to continue scanning
+//     char* size = strtok(NULL, breaker);
+//     char* filename = strtok(NULL, breaker);
+
+//     //now turn strings to int values
+//     unsigned int Total_frag = atoi(total_frag);
+//     unsigned int Frag_no = atoi(frag_no);
+//     unsigned int dSize = atoi(size);
+
+//     /*if(Frag_no != (prev_index) + 1){
+//         return -1;
+//     }*/
+
+//     if(Frag_no == 1){
+//         //this is the first message revieved, need to create a Packet struct and file stream
+//         Packet recieved;
+//         recieved.filename = filename;
+//         recieved.frag_no = Frag_no;
+//         recieved.size = dSize;
+//         recieved.total_frag = Total_frag;
+
+//         //create file stream, open a file
+//         FD = fopen(filename, "wb"); //recieved file needs to have same name as sent one
+//         if(FD == NULL){
+//             printf("Cannot create file\n"); 
+//             return -1; // -1 for error
+//         }
+//     }
+
+//     //now, write data into the created file use fwrite to write to stream
+//     int header_size = strlen(total_frag) + strlen(frag_no) + strlen(size) + strlen(filename);
+//     //fwrite, notice need to skip the header
+//     fwrite(message + header_size + sizeof(char) * 4, sizeof(char), dSize, FD);
+
+//     //now check if the last message is done transmitting
+//     if(Frag_no == Total_frag){
+//         //fclose(fd);
+//         return 1; // 1 for end of this file transmission
+//     }
+
+//     return 0; // 0 for success and continue this file transmission
+// }
+
+// void message_from_packet(Packet packet, char* message) {
+//     int index = sprintf(message, "%d:%d:%d:%s:", packet.total_frag, 
+//             packet.frag_no, packet.size, packet.filename);  // fill in everything except data region
+//     for(int i=0; i<packet.size; i++) {
+//         message[index+i] = packet.filedata[i];  // fill in the data region
+//     }
+// }
+
+// void constructPacketsArray(Packet* array, int total_packets, char* data, char* fileName, int remaining_file) {
+//     for(int index=0; index<total_packets; index++) {
+//         array[index].total_frag = total_packets;
+//         array[index].frag_no = index+1;
+
+//         /* pay attention to the last packet's size */
+//         if(index==total_packets-1 && remaining_file!=0) {
+//             array[index].size = remaining_file;
+//         } else {
+//             array[index].size = max_package_size;
+//         }
+
+//         array[index].filename = fileName;
+
+//         if(index==total_packets-1 && remaining_file!=0) {
+//             for(int i=0; i<remaining_file; i++) {
+//                 array[index].filedata[i] = data[i+index*max_package_size];
+//             }
+//         } else {
+//             for(int i=0; i<max_package_size; i++) {
+//                 array[index].filedata[i] = data[i+index*max_package_size];
+//             }
+//         }
+//     }
+// }
